1fb769c2523cba37d61e56519af0797321674f65
GuavaX <s9544033@yahoo.com.tw>
2019-12-20 14:23:10 +0800

    作業7_金幣產生機

---
+++ UnityProBuilderAnimation/Assets/Script/BornCoin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BornCoin : MonoBehaviour
+{
+    [Header("水晶幣")]
+    public GameObject coin;
+       
+
+    public void BurnCoin()
+    {
+        //aud.PlayOneShot(soundCoinBorn);
+
+        Vector3 pos = new Vector3(0, 0, 0);
+        Quaternion rot = new Quaternion(0, Random.Range(0,360), 0, 0);
+        Instantiate(coin,pos,rot);
+    }
+    
+}
